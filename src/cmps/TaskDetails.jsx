5873f688c12bf4e06dfb4bd237b41fa2eab82f7b
Ran Hirschorn <ranhirschorn@gmail.com>
2025-12-17 16:16:17 +0200

    asadsa

--- src/cmps/TaskDetails.jsx
+++ src/cmps/TaskDetails.jsx
@@ -1,3 +1,21 @@
-export function TaskDetails() {
+import { taskService } from '../services/task'
+import { Link, useNavigate, useParams } from "react-router-dom"
+import { useEffect } from 'react'
+import { showErrorMsg, showSuccessMsg } from "../services/event-bus.service.js"
+import { utilService } from "../services/util.service.js"
 
+
+export function TaskDetails() {
+    const { boardId, groupId, taskId } = useParams()
+    const [task, setTask] = useState(null)
+
+    useEffect(() => {
+        taskService.getTask(boardId, groupId, taskId).then(setTask)
+    }, [boardId, groupId, taskId])
+
+    return (
+        <div>
+            <h1>Task Details</h1>
+        </div>
+    )
 }
