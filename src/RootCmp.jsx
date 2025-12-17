8f891c6df4505a5ec96488e750e949dc0da4868a
asyakandyba <asyakan03@gmail.com>
2025-12-17 14:44:14 +0200

    Add cmps

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { Routes, Route } from 'react-router'
 
 import { HomePage } from './pages/HomePage'
-import { AboutUs, AboutTeam, AboutVision } from './pages/AboutUs'
-import { CarIndex } from './pages/CarIndex.jsx'
-import { ReviewIndex } from './pages/ReviewIndex.jsx'
-import { ChatApp } from './pages/Chat.jsx'
-import { AdminIndex } from './pages/AdminIndex.jsx'
+// import { AboutUs, AboutTeam, AboutVision } from './pages/AboutUs'
+// import { CarIndex } from './pages/CarIndex.jsx'
+// import { ReviewIndex } from './pages/ReviewIndex.jsx'
+// import { ChatApp } from './pages/Chat.jsx'
+// import { AdminIndex } from './pages/AdminIndex.jsx'
 
-import { CarDetails } from './pages/CarDetails'
-import { UserDetails } from './pages/UserDetails'
+// import { CarDetails } from './pages/CarDetails'
+// import { UserDetails } from './pages/UserDetails'
 
 import { AppHeader } from './cmps/AppHeader'
 import { AppFooter } from './cmps/AppFooter'
@@ -17,6 +17,9 @@ import { UserMsg } from './cmps/UserMsg.jsx'
 import { LoginSignup } from './pages/LoginSignup.jsx'
 import { Login } from './pages/Login.jsx'
 import { Signup } from './pages/Signup.jsx'
+
+import { BoardIndex } from './pages/BoardIndex'
+import { BoardDetails } from './pages/BoardDetails'
 
 export function RootCmp() {
     return (
@@ -27,16 +30,12 @@ export function RootCmp() {
             <main>
                 <Routes>
                     <Route path="" element={<HomePage />} />
-                    <Route path="about" element={<AboutUs />}>
+                    {/* <Route path="about" element={<AboutUs />}>
                         <Route path="team" element={<AboutTeam />} />
                         <Route path="vision" element={<AboutVision />} />
-                    </Route>
-                    <Route path="car" element={<CarIndex />} />
-                    <Route path="car/:carId" element={<CarDetails />} />
-                    <Route path="user/:id" element={<UserDetails />} />
-                    <Route path="review" element={<ReviewIndex />} />
-                    <Route path="chat" element={<ChatApp />} />
-                    <Route path="admin" element={<AdminIndex />} />
+                    </Route> */}
+                    <Route path="board" element={<BoardIndex />} />
+                    <Route path="board/:boardId" element={<BoardDetails />} />
                     <Route path="login" element={<LoginSignup />}>
                         <Route index element={<Login />} />
                         <Route path="signup" element={<Signup />} />
