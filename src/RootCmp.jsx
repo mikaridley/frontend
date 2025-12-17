159d7af8e49d7b83a94c6272e4d27559f16e2449
Mika Ridley <mikaridley26@gmail.com>
2025-12-17 15:58:02 +0200

    Basic css

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -13,21 +13,21 @@ import { HomePage } from './pages/HomePage'
 
 import { AppHeader } from './cmps/AppHeader'
 import { AppFooter } from './cmps/AppFooter'
-import { UserMsg } from './cmps/UserMsg.jsx'
-// import { LoginSignup } from './pages/LoginSignup.jsx'
-// import { Login } from './pages/Login.jsx'
-// import { Signup } from './pages/Signup.jsx'
+import { UserMsg } from './cmps/UserMsg'
+import { LoginSignup } from './pages/LoginSignup.jsx'
+import { Login } from './pages/Login.jsx'
+import { Signup } from './pages/Signup.jsx'
 
 import { BoardIndex } from './pages/BoardIndex'
 import { BoardDetails } from './pages/BoardDetails'
 
 export function RootCmp() {
   return (
-    <div className="main-container">
+    <div>
       <AppHeader />
       <UserMsg />
 
-      <main>
+      <main className="main-layout">
         <Routes>
           <Route path="" element={<HomePage />} />
           {/* <Route path="about" element={<AboutUs />}>
@@ -36,15 +36,13 @@ export function RootCmp() {
                     </Route> */}
           <Route path="board" element={<BoardIndex />} />
           <Route path="board/:boardId" element={<BoardDetails />} />
+          <Route path="login" element={<LoginSignup />}>
+            <Route index element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+          </Route>
         </Routes>
       </main>
       <AppFooter />
     </div>
   )
-}
-{
-  /* <Route path="login" element={<LoginSignup />}>
-            <Route index element={<Login />} />
-            <Route path="signup" element={<Signup />} />
-          </Route> */
 }
