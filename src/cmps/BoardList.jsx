be1d9c8d38da0d752c4af7068cbbe3c0f03a80d1
Mika Ridley <mikaridley26@gmail.com>
2025-12-18 13:31:33 +0200

    Add star sections

--- src/cmps/BoardList.jsx
+++ src/cmps/BoardList.jsx
@@ -5,27 +5,46 @@ import { useSelector } from 'react-redux'
 import { Link, Outlet } from 'react-router-dom'
 
 export function BoardList({ boards, addBoard, removeBoard, starToggle }) {
+  const starBoards = boards.filter(board => board.isStarred)
+  const notStarBoards = boards.filter(board => !board.isStarred)
   return (
     <section className="board-list">
-      {boards.map(board => {
-        return (
-          <BoardPreview
-            key={board._id}
-            board={board}
-            removeBoard={removeBoard}
-            starToggle={starToggle}
-          />
-        )
-      })}
-      <section className="add-board-card-container">
-        <Link
-          className="add-board-card"
-          to="/board/add-board"
-          state={{ origin: 'board-list' }}
-        >
-          Create new board
-        </Link>
-        <Outlet context={{ addBoard }} />
+      <h2 className="star-boards-header">Starred boards</h2>
+      <section className="star-boards">
+        {starBoards.map(board => {
+          return (
+            <BoardPreview
+              key={board._id}
+              board={board}
+              removeBoard={removeBoard}
+              starToggle={starToggle}
+            />
+          )
+        })}
+      </section>
+      <h2>Boards</h2>
+      <section className="not-star-boards">
+        {notStarBoards.map(board => {
+          return (
+            <BoardPreview
+              key={board._id}
+              board={board}
+              removeBoard={removeBoard}
+              starToggle={starToggle}
+            />
+          )
+        })}
+
+        <section className="add-board-card-container">
+          <Link
+            className="add-board-card"
+            to="/board/add-board"
+            state={{ origin: 'board-list' }}
+          >
+            Create new board
+          </Link>
+          <Outlet context={{ addBoard }} />
+        </section>
       </section>
     </section>
   )
