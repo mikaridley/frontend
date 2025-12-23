0b7b2304bd4f93517f69c4265de5451f25492c38
Mika Ridley <mikaridley26@gmail.com>
2025-12-23 16:58:21 +0200

    Finish HomePage

--- src/cmps/HomePageHeader.jsx
+++ src/cmps/HomePageHeader.jsx
@@ -1,9 +1,21 @@
 import { Link } from 'react-router-dom'
 import logoDarkImg from '../assets/img/logo-dark.png'
+import { useEffect, useState } from 'react'
 
 export function HomePageHeader() {
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  useEffect(() => {
+    const onScroll = () => {
+      setIsScrolled(window.scrollY > 0)
+    }
+
+    window.addEventListener('scroll', onScroll)
+    return () => window.removeEventListener('scroll', onScroll)
+  }, [])
+
   return (
-    <section className="home-page-header">
+    <section className={`home-page-header ${isScrolled ? 'scrolled' : ''}`}>
       <div className="header-content">
         <img src={logoDarkImg} />
         <nav>
